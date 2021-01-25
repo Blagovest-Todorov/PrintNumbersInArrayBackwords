81dec0d7d9e65a59943819ef024fe7c1328c9f10
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-01-25 16:06:06 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PrintNumbersReversly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine()); // read n numbers from the Console and reate such long an array
+            int[] numbers = new int[num];
+
+            for (int i = numbers.Length-1 ; i >= 0 ; i--)
+            {
+                int currNumber = int.Parse(Console.ReadLine());
+                numbers[i] = currNumber;
+
+            }
+            foreach (int item in numbers)
+            {
+                Console.Write($"{item} ");
+            }
+            
+        }
+    }
+}
